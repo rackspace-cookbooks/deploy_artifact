88df94d6100f94abcd726ce63a9814d117263870
Justin Seubert <dude051@gmail.com>
2015-11-22 15:22:28 -0600

    Fix and re-writes
    - Added handling of @LongLink tar entries
    - Fix for compare method
    - Fix for removing stale current/releases
    - Fix to de_deploy only when releases directory is empty
    - Removed direct deploy to current directory when keep_releases false
      due to being infeasible
    - Re-write untar method to be less complex adding helper methods for
      file_open, tar_open and gzip_stream

--- test/integration/tarball/serverspec/keep_releases_spec.rb
+++ test/integration/tarball/serverspec/keep_releases_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 require 'digest/md5'
 
 describe file('/var/keep_releases/current') do
-  it { should be_directory }
+  it { should be_symlink }
 end
 
 describe file('/var/keep_releases/cached-copy/keep_releases.tar.gz') do
   it { should be_file }
+end
+
+cached_checksum = Digest::MD5.hexdigest(File.read('/var/keep_releases/cached-copy/keep_releases.tar.gz'))
+describe file("/var/keep_releases/releases/#{cached_checksum}") do
+  it { should be_directory }
 end
 
 describe command('tar --compare -f /var/keep_releases/cached-copy/keep_releases.tar.gz \
