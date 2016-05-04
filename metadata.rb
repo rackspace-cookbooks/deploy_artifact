3add3ce1c72956aeee9fc0418c7d0c5ad25e2479
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2016-05-04 09:22:33 +0100

    Foodcritic corrections

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'A cookbook with library to deploy local files'
+issues_url 'https://github.com/rackspace-cookbooks/deploy_artifact/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/rackspace-cookbooks/deploy_artifact' if respond_to?(:source_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.0'
 
